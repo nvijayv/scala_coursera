31c0f17fd214058353af1a87f968db203d0ae295
Nitesh Vijayvargiya <nitesh9vijay@gmail.com>
2017-08-01 17:49:51 +0530

    indent 2 => 4

--- scala-spark-big-data/wikipedia/src/main/scala/wikipedia/WikipediaData.scala
+++ scala-spark-big-data/wikipedia/src/main/scala/wikipedia/WikipediaData.scala
@@ -4,17 +4,17 @@ import java.io.File
 
 object WikipediaData {
 
-  private[wikipedia] def filePath = {
-    val resource = this.getClass.getClassLoader.getResource("wikipedia/wikipedia.dat")
-    if (resource == null) sys.error("Please download the dataset as explained in the assignment instructions")
-    new File(resource.toURI).getPath
-  }
+    private[wikipedia] def filePath = {
+        val resource = this.getClass.getClassLoader.getResource("wikipedia/wikipedia.dat")
+        if (resource == null) sys.error("Please download the dataset as explained in the assignment instructions")
+        new File(resource.toURI).getPath
+    }
 
-  private[wikipedia] def parse(line: String): WikipediaArticle = {
-    val subs = "</title><text>"
-    val i = line.indexOf(subs)
-    val title = line.substring(14, i)
-    val text  = line.substring(i + subs.length, line.length-16)
-    WikipediaArticle(title, text)
-  }
+    private[wikipedia] def parse(line: String): WikipediaArticle = {
+        val subs = "</title><text>"
+        val i = line.indexOf(subs)
+        val title = line.substring(14, i)
+        val text  = line.substring(i + subs.length, line.length-16)
+        WikipediaArticle(title, text)
+    }
 }
